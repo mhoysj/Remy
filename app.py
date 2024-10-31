385398989dafe620720ad875c523a66b32a031c9
mhoysj <mhoyosj2@eafit.edu.co>
2024-10-31 10:49:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import platform
 #from pathlib import Path
 
 st.title('Generación Aumentada por Recuperación (RAG) 💬')
-image = Image.open('remylogo.png')
+image = Image.open('remylogo.jpg')
 st.write("Versión de Python:", platform.python_version())
 st.image(image, width=350)
 with st.sidebar:
